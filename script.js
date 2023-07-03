a48261c4d4c13007228e2d2c40bcd46c357a52a0
suleiman yusuf <108791588+hetrox8@users.noreply.github.com>
2023-07-03 19:19:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
+  // Retrieve book collection from localStorage or create a new empty array
+    var books = JSON.parse(localStorage.getItem("books")) || [];
 
+    // Function to render the book list on the page
+    function renderBookList() {
+      var bookList = document.getElementById("bookList");
+      bookList.innerHTML = ""; // Clear the existing list
+
+      books.forEach(function(book) {
+        var listItem = document.createElement("li");
+        listItem.textContent = book.title + " by " + book.author;
+
+        var removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.addEventListener("click", function() {
+          removeBook(book);
+        });
+
+        listItem.appendChild(removeButton);
+        bookList.appendChild(listItem);
+      });
+    }
+
+    // Function to add a new book to the collection
+    function addBook(title, author) {
+      var newBook = { title: title, author: author };
+      books.push(newBook);
+      localStorage.setItem("books", JSON.stringify(books));
+      renderBookList();
+    }
+
+    // Function to remove a book from the collection
+    function removeBook(book) {
+      books = books.filter(function(b) {
+        return b !== book;
+      });
+      localStorage.setItem("books", JSON.stringify(books));
+      renderBookList();
+    }
+
+    // Event listener for the addForm submit event
+    var addForm = document.getElementById("addForm");
+    addForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      var titleInput = document.getElementById("titleInput");
+      var authorInput = document.getElementById("authorInput");
+      var title = titleInput.value;
+      var author = authorInput.value;
+      addBook(title, author);
+      titleInput.value = "";
+      authorInput.value = "";
+    });
+
+    // Initial rendering of the book list
+    renderBookList();
